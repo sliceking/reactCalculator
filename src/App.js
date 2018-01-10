a505b928bd664bf537829f3e07a0fa4c31cfdeef
stan wielga <svwielga4@gmail.com>
2018-01-09 17:58:23 -0800

    adding project files

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import CalculatorFace from './components/calculator_face';
+
+class App extends Component {
+  render() {
+    return (
+      <CalculatorFace />
+    );
+  }
+}
+
+export default App;
---
+++ src/components/calculator_face.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class CalculatorFace extends Component {
+    render(){
+        return(
+            <div className="calculatorBody">Calculator Body</div>
+        );
+    }
+}
+
+export default CalculatorFace;
